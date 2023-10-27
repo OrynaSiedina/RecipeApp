5505f1a121fdbdf27132042f27adaf7f57c6ef01
Arina Siedina <orynasiedina@gmail.com>
2023-10-27 20:32:18 +0200

    Components for routes

---
+++ src/views/index.js
@@ -0,0 +1,6 @@
+import Home from './Home.vue'
+import About from './About.vue'
+import Recipes from './Recipes.vue'
+import Download from './Download.vue'
+import Contact from './Contact.vue'
+export { Home, About, Recipes, Download, Contact }
