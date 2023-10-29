2324b390cce58bc13cb4074fecc45c7b471855fa
Arina Siedina <orynasiedina@gmail.com>
2023-10-29 10:29:25 +0100

    SearchByName functionality

---
+++ src/components/MealItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="bg-bkg shadow rounded-xl hover:scale-105 transition-all text-content"
+  >
+    <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
+      <img
+        :src="meal.strMealThumb"
+        :alt="meal.strMeal"
+        class="rounded-t-xl w-full h-64 object-top object-cover"
+      />
+      <div class="p-2">
+        <h3 class="font-bold text-3xl text-center pb-2">{{ meal.strMeal }}</h3>
+       <p class="text-right">Click for Details</p>
+
+      </div>
+    </router-link>
+
+
+  </div>
+</template>
+
+<script setup>
+/*
+import YouTubeButton from './YouTubeButton.vue';
+*/
+import {truncateWords} from "../filters/index.js";
+
+const {meal} = defineProps({
+  meal: {
+    required: true,
+    type: Object
+  }
+})
+
+</script>
--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="flex justify-center">
-  <h1 class="text-4xl text-accent-1 font-bold mb-4">SEARCH RECIPE BY:</h1>
+  <div class="flex justify-center">
+    <h1 class="text-4xl text-accent-1 font-bold mb-4">SEARCH RECIPE BY:</h1>
   </div>
 
   <div>
@@ -10,31 +10,36 @@
         :key="index"
         @click="activeTab = index"
         :class="[
-          'cursor-pointer p-2 border-t border-l border-r border-accent-2 rounded-t text-accent-1 font-bold',
-          activeTab === index ? 'bg-accent-2 text-bkg' : '',
-        ]"
+        'cursor-pointer p-2 border-t border-l border-r border-accent-2 rounded-t text-accent-1 font-bold',
+        activeTab === index ? 'bg-accent-2 text-bkg' : '',
+      ]"
       >
         {{ tab.label }}
       </div>
     </div>
     <div class=" border border-accent-2 p-4 bg-accent-2 text-bkg">
       <div v-for="(tab, index) in tabs" :key="index">
-        <template v-if="activeTab === index">
-          {{ tab.content }}
-        </template>
+        <!-- Use the dynamic component based on the activeTab -->
+        <component :is="activeComponent"></component>
       </div>
     </div>
   </div>
 
 </template>
 <script setup>
-import { ref } from 'vue';
+import {ref} from 'vue';
+import MealsByName from '../components/MealsByName.vue';
+import {computed} from "@vue/reactivity";
 
 const activeTab = ref(0);
 const tabs = [
-  { label: 'KEY WORD', content: 'Content for Item One' },
-  { label: 'LETTER', content: 'Content for Item Two' },
-  { label: 'AREA', content: 'Content for Item Three' },
+  {label: "NAME", component: "MealsByName"},
+  {label: "LETTER", component: "MealsByName"},
+  {label: "ORIGIN", component: "MealsByName"},
 ];
 
+const activeComponent = computed(() => {
+  const componentName = tabs[activeTab.value].component;
+  return MealsByName;
+});
 </script>
