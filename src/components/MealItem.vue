a457a2e2974cd7d1d3031bc66a41c41eeb4556af
Arina Siedina <orynasiedina@gmail.com>
2023-10-30 16:49:32 +0100

    MealsItem style update

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="bg-bkg shadow rounded-xl hover:scale-105 transition-all text-content"
-  >
+  <div class="bg-bkg shadow rounded-xl hover:scale-105 transition-all text-content relative">
     <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
       <img
         :src="meal.strMealThumb"
@@ -9,21 +7,15 @@
         class="rounded-t-xl w-full h-64 object-top object-cover"
       />
       <div class="p-2">
-        <h3 class="font-bold text-3xl text-center pb-2">{{ meal.strMeal }}</h3>
-       <p class="text-right">Click for Details</p>
-
+        <h3 class="font-bold text-3xl text-center pb-8">{{ meal.strMeal }}</h3>
+        <p class="text-right absolute bottom-0 right-0 p-2">Click for Details</p>
       </div>
     </router-link>
-
-
   </div>
 </template>
 
+
 <script setup>
-/*
-import YouTubeButton from './YouTubeButton.vue';
-*/
-import {truncateWords} from "../filters/index.js";
 
 const {meal} = defineProps({
   meal: {
