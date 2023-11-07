99abc8978bd74ad0c8702b925dd72973686c4ec3
Arina Siedina <orynasiedina@gmail.com>
2023-11-07 17:23:38 +0100

    animation on enter for list of meals

--- src/components/Meals.vue
+++ src/components/Meals.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8">
+
+    <TransitionGroup tag="div" class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8" name="list">
     <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal"/>
-  </div>
+    </TransitionGroup>
+
   <div v-if="!meals.length" class="flex justify-center text-gray-600 p-8">
     There are no meals
   </div>
@@ -10,11 +12,30 @@
 <script setup>
 import MealItem from './MealItem.vue';
 
-const {meals} = defineProps({
+const { meals } = defineProps({
   meals: {
     required: true,
     type: Array,
   }
-})
+});
 
 </script>
+
+<style scoped>
+.list-enter-from{
+  opacity: 0;
+  transform: scale(0.5);
+}
+.list-enter-to{
+  opacity: 1;
+  transform: scale(1);
+}
+.list-enter-active {
+  transition: all 0.5s ease;
+}
+
+</style>
+
+
+
+
